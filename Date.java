dc8cf7387ce2289f663d3b9b5ac6f2a864b1babc
barracharles-glitch <barra.charles@student.ccm.edu>
2026-09-19 23:39:23 -0400

    9/19/2026

--- Date.java
+++ Date.java
@@ -1,7 +1,7 @@
 public class Date {
-    private String month="", day="", year="";
+    private int month, day, year;
 
-    public Date (String m, String d, String y){
+    public Date (int m, int d, int y){
         month = m;
         day = d;
         year = y;
@@ -10,32 +10,69 @@ public class Date {
     public Date (){
     }
 
-    public void setMonth(String m){
+    public void setMonth(int m){
         month = m;
     }
 
-    public void setDay(String d){
+    public void setDay(int d){
         day = d;
     }
 
-    public void setYear(String y){
+    public void setYear(int y){
         year = y;
     }
 
-    public String getMonth(){
+    public int getMonth(){
         return month;
     }
 
-    public String getDay(){
+    public int getDay(){
         return day;
     }
 
-    public String getYear(){
+    public int getYear(){
         return year;
     }
 
-    public String printDate1(String m, String y, String d){
-         return (m + "/" + y + "/" + d);
+    public String formatDate(int m, int d, int y){
+        return (m + "/" + d + "/" + y);
     }
 
+    public String formatDate2(int m, int y, int d){
+        return (numToMonth(m) + "/" + d + "/" + y);
+    }
+
+    public String formatDate3(int m, int y, int d){
+        return (d + "/" + numToMonth(m) + "/" + y);
+    }
+
+    public static String numToMonth(int m){
+        if (m == 1)
+            return "January";
+        else if (m == 2)
+            return "Febuary";
+        else if (m == 3)
+            return "March";
+        else if (m == 4)
+            return "April";
+        else if (m == 5)
+            return "May";
+        else if (m == 6)
+            return "June";
+        else if (m == 7)
+            return "July";
+        else if (m == 8)
+            return "August";
+        else if (m == 9)
+            return "September";
+        else if (m == 10)
+            return "October";
+        else if (m == 11)
+            return "November";
+        else if (m == 12)
+            return "December";
+
+        return "Not A Month";
+    }
 }
+
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class DateTest {
-    
+    public static void main(String[]args){
+        int month, day, year;
+        Scanner int_scan = new Scanner(System.in);
+
+        do{
+            System.out.print("Enter the month(1-12): ");
+            month = int_scan.nextInt();
+            if (month > 12 || month < 1)
+                System.out.println("Not a month. Enter a month 1-12. ");
+        }while(month > 12 || month < 1);
+
+        do{
+            System.out.print("Enter the day(1-31): ");
+            day = int_scan.nextInt();
+            if (day > 31 || day < 1)
+                System.out.println("Not a day. Enter a day 1-31. ");
+        }while(day > 31 || day < 1);
+
+        System.out.print("Enter the year: ");
+        year = int_scan.nextInt();
+
+
+        Date date1 = new Date(month, day, year);
+
+        System.out.println(date1.formatDate(month, day, year) + "\n" + 
+        date1.formatDate2(month, year, day) + "\n" + 
+        date1.formatDate3(month, year, day));
+
+        int_scan.close();
+    }
 }
